6ad883e4c5f0408a2f2042dbab2bea38ac9fe68b
Z_Shobbar <zoha.shobbar@outlook.com>
2023-05-19 15:06:55 +0330

    add insurance controller

---
+++ src/Jumbula.Api/Controllers/InsuranceController.cs
@@ -0,0 +1,12 @@
+﻿using Jumbula.Api.Controllers.Common;
+using Jumbula.Application.Dtos;
+using Jumbula.Application.Services.EntityServices.Common;
+using Jumbula.Domain.Entities;
+
+namespace Jumbula.Api.Controllers;
+public class InsuranceController : BaseController<Insurance, InsuranceInputDto>
+{
+    public InsuranceController(IBaseService<Insurance, InsuranceInputDto> service) : base(service)
+    {
+    }
+}
---
+++ src/Jumbula.Application/Dtos/InsuranceInputDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Jumbula.Application.Dtos;
+public class InsuranceInputDto
+{
+    public Guid ExternalId { get; set; }
+    public string CompanyName { get; set; }
+    public string PolicyNumber { get; set; }
+    public string CompanyPhone { get; set; }
+}
