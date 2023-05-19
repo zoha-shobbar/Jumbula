22b29932d8701979d6cdd1a29ea4d5bda59e4268
Z_Shobbar <zoha.shobbar@outlook.com>
2023-05-19 18:03:00 +0330

    add register parent test

--- src/Jumbula.Infrastructure/Data/DataInitializer.cs
+++ src/Jumbula.Infrastructure/Data/DataInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Jumbula.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jumbula.Infrastructure.Data;
 public class DataInitializer
@@ -6,5 +7,25 @@ public class DataInitializer
     public static void Initialize(DataContext context)
     {
         context.Database.Migrate();
+        SeedData(context);
+    }
+
+    public static void SeedData(DataContext context)
+    {
+        bool isInsuranceExist = context.Set<Insurance>().Any();
+        if (!isInsuranceExist)
+        {
+            Insurance insurance = new()
+            {
+                Id = Guid.Parse("4c730f1e-d036-435f-9ecb-3a4fbcbe1111"),
+                CompanyName = "Insurance 1",
+                CompanyPhone = "0935935935",
+                ExternalId = Guid.NewGuid(),
+                PolicyNumber = "1",
+            };
+
+            context.Set<Insurance>().Add(insurance);
+            context.SaveChanges();
+        }
     }
 }
--- test/Jumbula.Test/ApiTests/AccountApiTests.cs
+++ test/Jumbula.Test/ApiTests/AccountApiTests.cs
@@ -14,7 +14,7 @@ public class AccountApiTests : BaseTest
 {
     [Theory]
     [InlineData("b10@test.com", "b1@test.com", "a0762acd-8f24-417e-86e0-41d70e9d6b7d")]
-    public async Task RegisterParent(string email, string password, string externalId)
+    public async Task RegisterBusiness(string email, string password, string externalId)
     {
         SignUpBusinessInputDto input = new()
         {
@@ -39,29 +39,55 @@ public class AccountApiTests : BaseTest
     }
 
 
-    //[Theory]
-    //[InlineData("p1f1@test.com", "p1f1@test.com", "a0762acd-8f24-417e-86e0-41d70e9d6b7d")]
-    //public async Task RegisterBusiness(string email, string password, string externalId)
-    //{
-    //    SignUpBusinessInputDto input = new()
-    //    {
-    //        Email = email,
-    //        ExternalId = Guid.Parse(externalId),
-    //        Password = password
-    //    };
+    [Theory]
+    [InlineData("p10f10@test.com", "p10f10@test.com", "4c730f1e-d036-435f-9ecb-3a4fbcbe1111", "p10 f10",
+        "p10 f10 family", "Male", "2000-10-10", "0936123456789", "0936123456780",
+        1, 2, "address1", "address2", "New York", "USA",
+        "LA", "123456")]
+    public async Task RegisterParent(string email, string password, string insuranceId, string firstName,
+        string lastName, string gender, string dateOfBirth, string primaryPhone, string alternatePhone,
+        decimal lat, decimal lng, string address1, string address2, string city, string country,
+        string state, string zip)
+    {
+        SignUpParentInputDto input = new()
+        {
+            Email = email,
+            Password = password,
+            InsuranceId = Guid.Parse(insuranceId),
+            DateOfBirth = DateTime.Parse(dateOfBirth),
+            FirstName = firstName,
+            LastName = lastName,
+            Gender = gender,
+            ContactInfo = new()
+            {
+                PrimaryPhone = primaryPhone,
+                AlternatePhone = alternatePhone,
+                Address = new()
+                {
+                    Address1 = address1,
+                    Address2 = address2,
+                    City = city,
+                    Country = country,
+                    Lat = lat,
+                    Lng = lng,
+                    State = state,
+                    Zip = zip
+                }
+            }
+        };
 
-    //    var json = JsonConvert.SerializeObject(input);
-    //    var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        var json = JsonConvert.SerializeObject(input);
+        var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
 
-    //    var response = await TestClient.PostAsync("/api/Account/RegisterBusiness", stringContent);
+        var response = await TestClient.PostAsync("/api/Account/RegisterParent", stringContent);
 
-    //    response.EnsureSuccessStatusCode();
+        response.EnsureSuccessStatusCode();
 
-    //    var serializedResponse = await response.Content.ReadAsStringAsync();
-    //    var result = JsonConvert.DeserializeObject<SingleResponse<AccessToken>>(serializedResponse);
+        var serializedResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonConvert.DeserializeObject<SingleResponse<AccessToken>>(serializedResponse);
 
-    //    Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
-    //    Assert.Equal(ResponseStatus.Success, result.Status);
-    //    Assert.True(result.Result.Token.Length > 10);
-    //}
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(ResponseStatus.Success, result.Status);
+        Assert.True(result.Result.Token.Length > 10);
+    }
 }
