ff5633ee0e59b090cfe38c5ea724c95356684b87
Z_Shobbar <zoha.shobbar@outlook.com>
2023-05-19 17:10:57 +0330

    add base test

---
+++ test/Jumbula.Test/Common/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using Jumbula.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Jumbula.Test.Common;
+public class BaseTest
+{
+    protected readonly HttpClient TestClient;
+    protected BaseTest()
+    {
+        var appFactory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.RemoveAll(typeof(DataContext));
+                    services.AddDbContext<DataContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("Tests.Jumbula");
+                    });
+                });
+            });
+        TestClient = appFactory.CreateClient();
+    }
+}
